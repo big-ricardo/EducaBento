08692864aed6ee7c44bcca12762b376cc9c71310
Luis Ricardo Santos <rcsrcs1569@gmail.com>
2021-02-05 15:28:23 +0000

    up

--- pages/api/members.ts
+++ pages/api/members.ts
@@ -1,14 +1,37 @@
-import {NowRequest ,NowResponse} from '@vercel/node'
-import { Db, Collection} from 'mongodb'
+import { NowRequest, NowResponse } from '@vercel/node'
+import { Db, Collection } from 'mongodb'
 import url from 'url'
 import connectToDataBase from '../../utils/connectToDataBase'
 
-let cacheDb:Db = null
+let cacheDb: Db = null
 
-export default async (req:NowRequest, res:NowResponse) => {
+export default async (req: NowRequest, res: NowResponse) => {
 
-  const db = await connectToDataBase(cacheDb)
-  const members = await db.collection('members').find({authorID: {$ne: 0}}).toArray()
+  const db: Db = await connectToDataBase(cacheDb)
+  if (req.method === "GET") {
 
-  return res.status(200).json(members)
+    const members = await db.collection('members').find({ authorID: { $ne: 0 } }).toArray()
+
+    return res.status(200).json(members)
+
+  } else {
+    if (req.method === "POST") {
+
+      const members = await db.collection('members').insertOne({
+        name: req.body.name,
+        ocupation: req.body.ocupation,
+        description: req.body.description,
+        avatar: `/${req.body.avatar}.jpg`,
+        authorID: await getNextSequenceValue('authorID', db)
+      })
+      return res.status(200).json(members)
+    }
+  }
+
 }
+
+async function getNextSequenceValue(sequenceName, db: Db) {
+  const sequenceDocument = await db.collection('counters').findOneAndUpdate({ _id: sequenceName }, { $inc: { sequence_value: 1 } }, { returnOriginal: false })
+  return sequenceDocument.value.sequence_value;
+}
+
