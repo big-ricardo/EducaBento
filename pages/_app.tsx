be39fe54d6df0189b3ad998f4ad51dfb352fa022
Luis Ricardo Santos <rcsrcs1569@gmail.com>
2021-02-06 19:42:36 +0000

    push

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,14 @@ import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from '../styles/global'
 import themes from '../styles/themes'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+
+Router.events.on('routeChangeStart', (url) => {
+  NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
 
