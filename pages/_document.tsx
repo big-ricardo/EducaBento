9429e0751513b08169da5fa95f05dc4fd677490d
Luis Ricardo Santos <rcsrcs1569@gmail.com>
2021-02-22 16:59:34 +0000

    push

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,55 +1,33 @@
 import React from 'react'
-import Document, {
-  DocumentInitialProps,
-  DocumentContext,
-  Html,
-  Head,
-  Main,
-  NextScript
-} from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
-
-export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
-    const sheet = new ServerStyleSheet()
+import NextDocument from 'next/document'
+import { ServerStyleSheet as StyledComponentSheets } from 'styled-components'
+import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/core/styles'
+export default class Document extends NextDocument {
+  static async getInitialProps (ctx) {
+    const styledComponentSheet = new StyledComponentSheets()
+    const materialUiSheets = new MaterialUiServerStyleSheets()
     const originalRenderPage = ctx.renderPage
-
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+          enhanceApp: App => props =>
+            styledComponentSheet.collectStyles(
+              materialUiSheets.collect(<App {...props} />),
+            ),
         })
-
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await NextDocument.getInitialProps(ctx)
       return {
         ...initialProps,
-        styles: (
-          <>
+        styles: [
+          <React.Fragment key="styles">
             {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        )
+            {materialUiSheets.getStyleElement()}
+            {styledComponentSheet.getStyleElement()}
+          </React.Fragment>,
+        ],
       }
     } finally {
-      sheet.seal()
+      styledComponentSheet.seal()
     }
-  }
-
-  render(): JSX.Element {
-    return (
-      <Html lang="pt">
-        <Head>
-          <meta charSet="utf-8" />
-          <link rel="preconnect" href="https://fonts.gstatic.com"/>
-          <link href="https://fonts.googleapis.com/css2?family=Philosopher:wght@400;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"></link>
-        </Head>
-          <body>
-            <Main />
-            <NextScript />
-          </body>
-      </Html>
-    )
   }
 }
